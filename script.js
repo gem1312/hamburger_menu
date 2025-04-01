fe3caa595c654b3635361068a7b1e8766dbc7439
gem1312 <gem13122002@gmail.com>
2025-04-01 20:50:08 +0200

    created hamburger-menu

---
+++ script.js
@@ -0,0 +1,7 @@
+
+const hamburger = document.getElementById('hamburger');
+const nav = document.querySelector('nav ul');
+
+hamburger.addEventListener('click', ()=> {
+    nav.classList.toggle('active');
+}); 
