4eedb557c849f00c10681b0be74c7e68b954e9cf
gem1312 <gem13122002@gmail.com>
2025-04-03 20:26:06 +0200

    pop-up and updates

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-
 const hamburger = document.getElementById('hamburger');
-const nav = document.querySelector('nav ul');
+const nav = document.querySelector('.list');
 
-hamburger.addEventListener('click', ()=> {
-    nav.classList.toggle('active');
-}); 
+hamburger.addEventListener('click', () => {
+    nav.classList.toggle('open');
+});
+
+
+const modal = document.getElementById("modal");
+const openModalBtn = document.getElementById("openModal");
+const closeModalBtn = document.querySelector(".close");
+
+openModalBtn.addEventListener("click", () => {
+    modal.classList.add("show");
+    document.body.classList.add("modal-open");
+});
+
+closeModalBtn.addEventListener("click", () => {
+    modal.classList.remove("show");
+    document.body.classList.remove("modal-open"); 
+});
+
